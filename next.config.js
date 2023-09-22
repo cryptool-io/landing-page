44e6658b85aab8c2c18e93dddadb17a6205bc494
secretsudev <secretsudev@gmail.com>
2023-09-22 09:45:40 +0200

    Fix config.js

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@ const nextConfig = {
 }
 
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
-};
-
-module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
@@ -23,7 +12,11 @@ module.exports = {
       config.resolve.fallback.net = false;
       config.resolve.fallback.child_process = false;
     }
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
 
     return config;
-  },
+  }
 };
